30ab28ebb8838895345db60d32ea9756cb1fbd3f
Eduardo Sánchez <eduardosanchez@MacBook.home>
2022-08-17 19:45:40 +0200

    Learning more about Hooks

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import './App.css';
 // import ContactList from './components/containers/Contact_List';
 // import Ejemplo1 from './hooks/Ejemplo1';
 // import Ejemplo2 from './hooks/Ejemplo2';
-import MiComponenteConContexto from './hooks/Ejemplo3';
+// import MiComponenteConContexto from './hooks/Ejemplo3';
+import Ejemplo4 from './hooks/Ejemplo4';
 
 function App() {
   return (
@@ -17,16 +18,20 @@ function App() {
         {/* <GreetingF name="Edu" /> */}
 
         {/* Componente de estado de las tareas */}
+        
         {/* <TaskListComponent></TaskListComponent> */}
         {/* <ContactList></ContactList> */}
-
-
 
         {/* EJEMPLOS DE DE USO DE HOOKS */}
 
         {/* <Ejemplo1></Ejemplo1> */}
         {/* <Ejemplo2></Ejemplo2> */}
-        <MiComponenteConContexto></MiComponenteConContexto>
+        {/* <MiComponenteConContexto></MiComponenteConContexto> */}
+
+        <Ejemplo4 nombre={ 'Edguard' }>
+          {/* Todo lo que hay aquí dentro se tratará como un prop.children */}
+          Contenido del props.children
+        </Ejemplo4>
       </header>
     </div>
   );
